70491b8d1910d877eaf437d5b74d6bd245f20553
NahuelCondori99 <nahuelcondori62t@gmail.com>
2025-10-29 22:37:00 -0300

    Creo mis metodos en la clase Presupuestos

--- Models/Presupuestos.cs
+++ Models/Presupuestos.cs
@@ -17,4 +17,35 @@ public class Presupuestos
     public string NombreDestinatario { get => nombreDestinatario; set => nombreDestinatario = value; }
     public DateTime FechaCreacion { get => fechaCreacion; set => fechaCreacion = value; }
     public List<PresupuestoDetalles> Detalles { get => detalles; set => detalles = value; }
+
+    public double MontoPresupuesto()
+    {
+        double total = 0;
+
+        foreach (var prod in detalles)
+        {
+            total += prod.Producto.Precio * prod.Cantidad;
+        }
+        return total;
+    }
+
+    public double MontoPresupuestoIVA()
+    {
+        double montoBase = MontoPresupuesto();
+        double iva = montoBase * 0.21;
+
+        return (montoBase * iva);
+
+    }
+
+    public int CantidadProductos()
+    {
+        int totalCantidad = 0;
+
+        foreach (var p in detalles)
+        {
+            totalCantidad += p.Cantidad;
+        }
+        return totalCantidad;
+    }
 }
